16562746e98b35513736fb2cc61fce0b687cb1fb
Lukas Budach <lukas.budach@online.de>
2022-05-27 17:25:28 +0200

    comments and some starting code for the dictionary segment processing

--- src/lib/operators/table_scan.cpp
+++ src/lib/operators/table_scan.cpp
@@ -113,7 +113,73 @@ std::shared_ptr<const Table> TableScan::_on_execute() {
         using Type = typename decltype(type)::type;
         const auto& dict_segment_ptr = std::dynamic_pointer_cast<DictionarySegment<Type>>(segment);
         if (dict_segment_ptr) {
-          const auto& values = dict_segment_ptr->attribute_vector();
+          /*
+           * Equal is trivial: if _search_value can't be encoded (i.e. does not exist in the raw attribute vector for the segment), there is no equal value; otherwise encode and compare
+           * !Equal is the same: if _search_value can't be encoded, there are no equal values, so all pass the filter; otherwise encode and compare
+           * less than: if _search_value can't be encoded: find next smaller raw value that was in the attribute vector, encode it and run a less-than-equal with that value
+           */
+
+          const auto typed_search_value = type_cast<Type>(_search_value);
+          const auto encoded_search_value = dict_segment_ptr->get_encoded_value(typed_search_value);
+          if (encoded_search_value == dict_segment_ptr->unique_values_count()) {
+            // can't encode value if return is equal to dictionary size (=n unique values)
+            switch (_scan_type) {
+              case ScanType::OpEquals:
+                // done here, no matching values if operator == Equal
+                break;
+              case ScanType::OpNotEquals:
+                // done here, all values match if operator == NotEqual
+                // add all value pointers to position list
+                for (const auto reference : *ref_segment->pos_list()) {
+                  if (reference.chunk_id == chunk_index) {
+                    pos_list->push_back(reference);
+                  }
+                }
+                break;
+              case ScanType::OpLessThan:
+              case ScanType::OpLessThanEquals: {
+                // lower bound gives us ID (encoded value) of the raw value that is the first one not smaller than the search value
+                // we want the raw value that is the next-smaller one, so substract one
+                auto next_smaller_value = dict_segment_ptr->lower_bound(typed_search_value);
+                if (next_smaller_value == INVALID_VALUE_ID) {
+                  // all values are smaller than search values; add them all to position list
+                  for (const auto reference : *ref_segment->pos_list()) {
+                    if (reference.chunk_id == chunk_index) {
+                      pos_list->push_back(reference);
+                    }
+                  }
+                } else if (next_smaller_value == 0) {
+                  // all values are larger than search value; we are done here as lt or leq do not find matches
+                } else {
+                  --next_smaller_value;
+                  // compare to all values with leq as this value is already less than search value
+                }
+                break;
+              }
+              case ScanType::OpGreaterThan:
+              case ScanType::OpGreaterThanEquals:
+              {
+                // lower bound gives us ID (encoded value) of the raw value that is the first one not smaller than the search value
+                // which is exactly what we want here (the next larger value)
+                auto next_larger_value = dict_segment_ptr->lower_bound(typed_search_value);
+                if (next_larger_value == INVALID_VALUE_ID) {
+                  // all values are smaller than search values; we are done here, none fit the filter
+                } else if (next_larger_value == 0) {
+                  // all values are larger than search value; add them all to the position list
+                  for (const auto reference : *ref_segment->pos_list()) {
+                    if (reference.chunk_id == chunk_index) {
+                      pos_list->push_back(reference);
+                    }
+                  }
+                } else {
+                  // compare to all values with geq as this value is already larger than search value
+                }
+                break;
+              }
+            }
+          } else {
+            // in this case, take the encoded value and do the comparison as usual
+          }
         } else {
           const auto& value_segment_ptr = std::dynamic_pointer_cast<ValueSegment<Type>>(segment);
           const auto& values = value_segment_ptr->values();
